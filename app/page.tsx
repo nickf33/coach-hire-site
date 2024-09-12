e64d2936d398d3e34089fb71a2fbc6afe2a602b3
nickfeltham33 <nick.feltham33@gmail.com>
2024-09-12 10:00:39 +0100

    removed links to blog

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import HeroWrap from "./_components/sections/hero/HeroWrap";
 import GridWrap from "./_components/sections/grid/GridWrap";
 import SliderWrap from "./_components/sections/slider/SliderWrap";
 import FAQWrap from "./_components/sections/faq/FAQWrap";
-import BlogWrap from "./_components/sections/blog/BlogWrap";
 import FooterWrap from "./_components/layout/footer/FooterWrap";
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <GridWrap />
       <SliderWrap />
       <FAQWrap />
-      {/* <BlogWrap /> */}
       <FooterWrap />
     </main>
   );
