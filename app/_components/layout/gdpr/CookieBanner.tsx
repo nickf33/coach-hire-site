d054c35724895d77b49dc6f94df92bfa521744df
nickfeltham33 <nick.feltham33@gmail.com>
2024-09-17 22:06:15 +0100

    suspense wrapper

--- app/_components/layout/gdpr/CookieBanner.tsx
+++ app/_components/layout/gdpr/CookieBanner.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { getLocalStorage, setLocalStorage } from "@/app/lib/storageHelper";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { Suspense } from "react";
 
 type CookiePreferences = {
   necessary: boolean;
@@ -67,76 +68,81 @@ export default function CookieBanner() {
   }
 
   return (
-    <div className="fixed rounded-xl bg-primary bottom-0 mx-auto my-10 py-3 w-3/5 max-w-[34rem] left-10 right-auto px-4 z-[9999]">
-      <div className="text-white p-2">
-        <Link href="/cookie-policy">
-          <h2>
-            We use <span className="font-bold text-accent">cookies</span> on our
-            site.
-          </h2>
-        </Link>
-        <p className="text-xs mt-4 mr-4">
-          This website utilises cookies to enable essential site functionality
-          and analytics. You may change your settings at any time or accept the
-          default settings. You may close this banner to continue with only
-          essential cookies.
-        </p>
-      </div>
-      <div className="flex items-start justify-between gap-4 rounded-lg py-2 md:flex-row xs:flex-col flex-row">
-        <div className="flex justify-between gap-5 xs:flex-row sm:w-60 w-full flex-col">
+    <Suspense>
+      <div className="fixed rounded-xl bg-primary bottom-0 mx-auto my-10 py-3 w-3/5 max-w-[34rem] left-10 right-auto px-4 z-[9999]">
+        <div className="text-white p-2">
+          <Link href="/cookie-policy">
+            <h2>
+              We use <span className="font-bold text-accent">cookies</span> on
+              our site.
+            </h2>
+          </Link>
+          <p className="text-xs mt-4 mr-4">
+            This website utilises cookies to enable essential site functionality
+            and analytics. You may change your settings at any time or accept
+            the default settings. You may close this banner to continue with
+            only essential cookies.
+          </p>
+        </div>
+        <div className="flex items-start justify-between gap-4 rounded-lg py-2 md:flex-row xs:flex-col flex-row">
+          <div className="flex justify-between gap-5 xs:flex-row sm:w-60 w-full flex-col">
+            <button
+              type="button"
+              className="rounded-md border border-secondary bg-secondary px-4 py-2 w-full text-sm text-white transition hover:bg-zinc-800 hover:border-white"
+              onClick={() => handleConsent(false)}
+            >
+              Decline All
+            </button>
+            <button
+              type="button"
+              className="rounded-md bg-accent px-4 py-2 text-sm text-white transition border w-full border-accent hover:bg-transparent hover:border"
+              onClick={() => handleConsent(true)}
+            >
+              Accept All
+            </button>
+          </div>
           <button
             type="button"
-            className="rounded-md border border-secondary bg-secondary px-4 py-2 w-full text-sm text-white transition hover:bg-zinc-800 hover:border-white"
-            onClick={() => handleConsent(false)}
+            className="rounded-md border border-secondary bg-secondary px-4 py-2 text-sm text-white sm:w-40 w-full transition hover:bg-zinc-800 hover:border-white"
+            onClick={handleManagePreferences}
           >
-            Decline All
-          </button>
-          <button
-            type="button"
-            className="rounded-md bg-accent px-4 py-2 text-sm text-white transition border w-full border-accent hover:bg-transparent hover:border"
-            onClick={() => handleConsent(true)}
-          >
-            Accept All
+            {showPreferences ? "Hide" : ""} Preferences
           </button>
         </div>
-        <button
-          type="button"
-          className="rounded-md border border-secondary bg-secondary px-4 py-2 text-sm text-white sm:w-40 w-full transition hover:bg-zinc-800 hover:border-white"
-          onClick={handleManagePreferences}
+        <div
+          className={`overflow-hidden transition-all duration-300 ease-in-out ${showPreferences ? "max-h-96" : "max-h-0"}`}
         >
-          {showPreferences ? "Hide" : ""} Preferences
-        </button>
-      </div>
-      <div
-        className={`overflow-hidden transition-all duration-300 ease-in-out ${showPreferences ? "max-h-96" : "max-h-0"}`}
-      >
-        <div className="text-white">
-          <h3 className="font-semibold my-4">Cookie Preferences</h3>
-          <hr className="mb-4" />
-          {(Object.keys(preferences) as Array<keyof CookiePreferences>).map(
-            (key) => (
-              <div key={key} className="flex items-center justify-between mb-4">
-                <label htmlFor={key} className="text-sm flex-grow">
-                  {key.charAt(0).toUpperCase() + key.slice(1)}
-                </label>
-                <ToggleSwitch
-                  isOn={preferences[key]}
-                  toggleSwitch={() => handlePreferenceChange(key)}
-                  disabled={key === "necessary"}
-                />
-              </div>
-            )
-          )}
-          <button
-            type="button"
-            onClick={handleSavePreferences}
-            className="mt-2 rounded-md bg-accent px-4 py-2 text-sm text-white transition border border-accent hover:bg-transparent hover:border"
-          >
-            Save Preferences
-          </button>
+          <div className="text-white">
+            <h3 className="font-semibold my-4">Cookie Preferences</h3>
+            <hr className="mb-4" />
+            {(Object.keys(preferences) as Array<keyof CookiePreferences>).map(
+              (key) => (
+                <div
+                  key={key}
+                  className="flex items-center justify-between mb-4"
+                >
+                  <label htmlFor={key} className="text-sm flex-grow">
+                    {key.charAt(0).toUpperCase() + key.slice(1)}
+                  </label>
+                  <ToggleSwitch
+                    isOn={preferences[key]}
+                    toggleSwitch={() => handlePreferenceChange(key)}
+                    disabled={key === "necessary"}
+                  />
+                </div>
+              )
+            )}
+            <button
+              type="button"
+              onClick={handleSavePreferences}
+              className="mt-2 rounded-md bg-accent px-4 py-2 text-sm text-white transition border border-accent hover:bg-transparent hover:border"
+            >
+              Save Preferences
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
